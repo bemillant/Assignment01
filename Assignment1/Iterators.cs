a3e762d04d9e175d01e86ace672d727e4c54d0b1
SpaceVikingEik <37214305+SpaceVikingEik@users.noreply.github.com>
2022-09-09 13:56:26 +0100

    Ugly Implementation of Flatten

--- Assignment1/Iterators.cs
+++ Assignment1/Iterators.cs
@@ -2,7 +2,17 @@ namespace Assignment1;
 
 public static class Iterators
 {
-    public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items) => throw new NotImplementedException();
+    public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items) {
+        //temp ugly solution, refactor this later
+        var UglyList = new List<T>(); 
+        foreach (IEnumerable<T> item in items)
+        {
+            foreach (T thing in item){
+                UglyList.Add(thing);
+            }
+        }
+        return UglyList;
+    }
 
-    public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate) => throw new NotImplementedException();
+    public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate) => throw new NotImplementedException(); 
 }
