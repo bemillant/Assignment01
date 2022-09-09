c42c2603d63692c9147ca5ad6ca81b01fc359a3a
Benjamin Millant <bemi@itu.dk>
2022-09-09 14:45:47 +0200

    Done with first two implementations.

--- Assignment1/Iterators.cs
+++ Assignment1/Iterators.cs
@@ -2,17 +2,39 @@ namespace Assignment1;
 
 public static class Iterators
 {
-    public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items) {
+    public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items)
+    {
         //temp ugly solution, refactor this later
-        var UglyList = new List<T>(); 
-        foreach (IEnumerable<T> item in items)
+        // var UglyList = new List<T>();
+        // foreach (IEnumerable<T> item in items)
+        // {
+        //     foreach (T thing in item)
+        //     {
+        //         UglyList.Add(thing);
+        //     }
+        // }
+        // return UglyList;
+
+        //Attempt at making it prettier:
+        foreach (var singleEnumerable in items)
         {
-            foreach (T thing in item){
-                UglyList.Add(thing);
+            foreach (var singleItem in singleEnumerable)
+            {
+                yield return singleItem;
             }
         }
-        return UglyList;
     }
 
-    public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate) => throw new NotImplementedException(); 
+    public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate)
+    {
+        foreach (var item in items)
+        {
+            if (predicate(item))
+            {
+                yield return item;
+            }
+        }
+    }
+
+
 }
