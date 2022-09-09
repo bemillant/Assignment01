ab0ddb4ec392d4c09b08403ed3ac520ee2150734
SpaceVikingEik <37214305+SpaceVikingEik@users.noreply.github.com>
2022-09-09 13:47:58 +0100

    Test is done

--- Assignment1.Tests/IteratorsTests.cs
+++ Assignment1.Tests/IteratorsTests.cs
@@ -1,20 +1,20 @@
 namespace Assignment1.Tests;
 
+
 public class IteratorsTests
 {
 
-    [[Fact]
+    [Fact]
     public void Flatten_streams_1_2_and_3_4_return_strea_1_2_3_4()
     {
         // Given
-        var iterator = new Iterator();
         var stream1 = new int[]{1,2};
         var stream2 = new int[]{3,4};
-        var streamMix = new nameof(stream1)[]{stream1,stream2};
+        var streamMix = new int[][]{stream1, stream2};
         // When
-        var flattenStream = iterator.Flatten(streamMix);
+        var flattenStream = Iterators.Flatten(streamMix);
 
         // Then
-        assert.Equals({1,2,3,4}, flattenStream);
-    }]
+        Assert.Equal(new int[]{1,2,3,4}, flattenStream);
+    }
 }
