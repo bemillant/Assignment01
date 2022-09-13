d2e08189e4424734d0dc6f2072f26d92a6894ad4
Benjamin Millant <bemi@itu.dk>
2022-09-13 17:10:44 +0200

    Works with simple tags - not div and p and such. I think the regex needs to be changed.

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -16,36 +16,51 @@ public class RegExprTests
         Assert.Equal(streamShouldBe, result);
     }
 
+    [Fact]
+    public void Resolutions_when_stream_contains_1920x1080_1024x768_800x600_returns_tuples()
+    {
+        // Given
+        var stream = new List<String> { "1920x1080", "1024x768, 800x600" };
+        (int, int) t1 = (1920, 1080);
+        (int, int) t2 = (1024, 768);
+        (int, int) t3 = (800, 600);
+        var streamToBe = new List<(int, int)> { t1, t2, t3 };
+
+        // When
+        var result = RegExpr.Resolution(stream);
+
+        // Then
+        Assert.Equal(streamToBe, result);
+    }
+
     // [Fact]
-    // public void Resolutions_when_stream_contains_1920x1080_1024x768_800x600_returns_tuples()
+
+    // public void InnerText_htmlInnerText_with_tag_a_should_return_stream_of_string()
     // {
     //     // Given
-    //     var stream = new List<String> { "1920x1080", "1024x768, 800x600" };
-    //     (int, int) t1 = (1920, 1080);
-    //     (int, int) t2 = (1024, 768);
-    //     (int, int) t3 = (800, 600);
-    //     var streamToBe = new List<(int, int)> { t1, t2, t3 };
+
+    //     String input = "<div> <p>A <b>regular expression</b>, <b>regex</b> or <b>regexp</b> (sometimes called a <b>rational expression</b>) is, in <a href=\"https://en.wikipedia.org/wiki/Theoretical_computer_science\" title=\"Theoretical computer science\">theoretical computer science</a> and <a href=\"https://en.wikipedia.org/wiki/Formal_language\" title=\"Formal language\">formal language</a> theory, a sequence of <a href=\"https://en.wikipedia.org/wiki/Character_(computing)\" title=\"Character (computing)\">characters</a> that define a <i>search <a href=\"https://en.wikipedia.org/wiki/Pattern_matching\" title=\"Pattern matching\">pattern</a></i>. Usually this pattern is then used by <a href=\"https://en.wikipedia.org/wiki/String_searching_algorithm\" title=\"String searching algorithm\">string searching algorithms</a> for \"find\" or \"find and replace\" operations on <a href=\"https://en.wikipedia.org/wiki/String_(computer_science)\" title=\"String (computer science)\">strings</a>.</p> </div>";
+    //     var streamToBe = new List<String> { "theoretical computer science", "formal language", "characters", "pattern", "string searching algorithms", "strings" };
 
 
     //     // When
-    //     var result = RegExpr.Resolution(stream);
+    //     var result = RegExpr.InnerText(input, "a");
 
     //     // Then
     //     Assert.Equal(streamToBe, result);
     // }
-
     [Fact]
 
-    public void InnerText_htmlInnerText_should_return_stream_of_string()
+    public void InnerText_htmlInnerText_with_tag_p_should_return_stream_of_string()
     {
         // Given
-        //var pattern = @"(?<startEle><(?<start>\w+)(.*?)?)(?<innerText>[\w \n]+)(?<end><\/\k<start>>)";
-        String input = "<div> <p>A <b>regular expression</b>, <b>regex</b> or <b>regexp</b> (sometimes called a <b>rational expression</b>) is, in <a href=\"https://en.wikipedia.org/wiki/Theoretical_computer_science\" title=\"Theoretical computer science\">theoretical computer science</a> and <a href=\"https://en.wikipedia.org/wiki/Formal_language\" title=\"Formal language\">formal language</a> theory, a sequence of <a href=\"https://en.wikipedia.org/wiki/Character_(computing)\" title=\"Character (computing)\">characters</a> that define a <i>search <a href=\"https://en.wikipedia.org/wiki/Pattern_matching\" title=\"Pattern matching\">pattern</a></i>. Usually this pattern is then used by <a href=\"https://en.wikipedia.org/wiki/String_searching_algorithm\" title=\"String searching algorithm\">string searching algorithms</a> for \"find\" or \"find and replace\" operations on <a href=\"https://en.wikipedia.org/wiki/String_(computer_science)\" title=\"String (computer science)\">strings</a>.</p> </div>";
-        var streamToBe = new List<String> { "theoretical computer science", "formal language", "characters", "pattern", "string searching algorithms", "strings" };
+
+        String input = @"<div> <p>The phrase <i>regular expressions</i> (and consequently, regexes) is often used to mean the specific, standard textual syntax for representing <u>patterns</u> that matching <em>text</em> need to conform to.</p> </div>";
+        var streamToBe = new List<String> { "theoretical computThe phrase regular expressions (and consequently, regexes) is often used to mean the specific, standard textual syntax for representing patterns that matching text need to conform to." };
 
 
         // When
-        var result = RegExpr.InnerText(input, "a");
+        var result = RegExpr.InnerText(input, "p");
 
         // Then
         Assert.Equal(streamToBe, result);
--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -6,85 +6,55 @@ public static class RegExpr
 {
     public static IEnumerable<string> SplitLine(IEnumerable<string> lines)
     {
-
-        //Can probably be done a lot prettier
-        var patternWithSpace = @"[\d\w ]+";
-        var patternWithoutSpace = @"[\d\w]+";
+        var pattern = @"[\d\w]+";
+        var reg = new Regex(pattern);
 
         foreach (var item in lines)
         {
-            var matchWithoutSpace = Regex.Match(item, patternWithoutSpace);
-            var matchWithSpace = Regex.Match(item, patternWithSpace);
-
-            if (matchWithSpace.Success)
+            foreach (var word in reg.Matches(item))
             {
-                var tempArr = item.Split(" ");
-                foreach (var word in tempArr)
-                {
-                    yield return word;
-                }
-            }
-            else if (matchWithoutSpace.Success)
-            {
-                yield return item;
+                var wordy = word.ToString();
+                yield return wordy;
             }
         }
     }
 
     public static IEnumerable<(int width, int height)> Resolution(IEnumerable<string> resolutions)
     {
-        //Kunne ikke lige regne den helt ud, nogle idéer til fremgangsmåde er nedenunder:
-        yield return (0, 0);
-        // {
-        //     var pattern = @"(?<first>[\d]+)x(?<second>[\d]+)*";
-        //     var reg = new Regex(pattern);
 
-        //     var allGroups = reg.GetGroupNames();
-
-        //     foreach (var item in resolutions)
-        //     {
-        //         var match = reg.Match(item);
-        //         foreach (var grp in allGroups)
-        //         {
-        //             var first = match.Groups["first"].Value;
-        //             var second = match.Groups["second"].Value;
-        //             Console.WriteLine(first + " " + second);
-        //             yield return (int.Parse(first), int.Parse(second));
-        //         }
-
-        //     }
-
-
-        //     // var patternWithoutSpace = @"(?<first>[\d]+)x(?<second>[\d]+)*";
-
-
-        //     // foreach (var item in resolutions)
-        //     // {
-        //     //     var matchWithoutSpace = Regex.Match(item, patternWithoutSpace);
-
-
-        //     //     if (matchWithoutSpace.Success)
-        //     //     {
-        //     //         if (item.Contains(" "))
-        //     //         {
-        //     //             var tempArr = item.Split(" ");
-
-        //     //             foreach (var res in tempArr)
-        //     //             {
-        //     //                 res.Replace(",", "");
-        //     //                 yield return (int.Parse(matchWithoutSpace.Groups["first"].Value), int.Parse(matchWithoutSpace.Groups["second"].Value));
-        //     //                 Console.WriteLine(res);
-        //     //             }
-        //     //         }
-        //     //         else
-        //     //         {
-        //     //             Console.WriteLine("aaaaa");
-        //     //             yield return (int.Parse(matchWithoutSpace.Groups["first"].Value), int.Parse(matchWithoutSpace.Groups["second"].Value));
-        //     //         }
-        //     //     }
-        //     // }
-
+        var pattern = @"(?<first>[\d]+)(?<eks>[x])(?<second>[\d]+)*";
+        var reg = new Regex(pattern);
+        foreach (var item in resolutions)
+        {
+            foreach (Match matches in reg.Matches(item))
+            {
+                var first = matches.Groups["first"].Value;
+                var second = matches.Groups["second"].Value;
+                yield return (int.Parse(first), int.Parse(second));
+            }
+        }
 
     }
-    public static IEnumerable<string> InnerText(string html, string tag) => throw new NotImplementedException();
+
+
+
+    public static IEnumerable<string> InnerText(string html, string tag)
+    {
+
+        //Works with simple tags - not div and p and such. I think the regex needs to be changed.
+        var matchesCol = Regex.Matches(html, @"(?<startEle><" + tag + @"(.*?)?>)(?<innerText>[\w \n]+)");
+
+        //var pattern = @"(?<startEle><(?<start>\w+)(.*?)?)(?<innerText>[\w \n]+)(?<end><\/\k<start>>)";
+        //var reg = new Regex(pattern);
+
+        foreach (Match match in matchesCol)
+        {
+            if (match.Success)
+            {
+                Console.WriteLine(match.Groups["innerText"].Value);
+                yield return match.Groups["innerText"].Value;
+            }
+
+        }
+    }
 }
