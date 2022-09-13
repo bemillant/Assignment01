4bfbf0673120f80df53574ba42c2462113c29714
Benjamin Millant <bemi@itu.dk>
2022-09-13 16:03:00 +0200

    added test

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -16,19 +16,36 @@ public class RegExprTests
         Assert.Equal(streamShouldBe, result);
     }
 
+    // [Fact]
+    // public void Resolutions_when_stream_contains_1920x1080_1024x768_800x600_returns_tuples()
+    // {
+    //     // Given
+    //     var stream = new List<String> { "1920x1080", "1024x768, 800x600" };
+    //     (int, int) t1 = (1920, 1080);
+    //     (int, int) t2 = (1024, 768);
+    //     (int, int) t3 = (800, 600);
+    //     var streamToBe = new List<(int, int)> { t1, t2, t3 };
+
+
+    //     // When
+    //     var result = RegExpr.Resolution(stream);
+
+    //     // Then
+    //     Assert.Equal(streamToBe, result);
+    // }
+
     [Fact]
-    public void Resolutions_when_stream_contains_1920x1080_1024x768_800x600_returns_tuples()
+
+    public void InnerText_htmlInnerText_should_return_stream_of_string()
     {
         // Given
-        var stream = new List<String> { "1920x1080", "1024x768, 800x600" };
-        (int, int) t1 = (1920, 1080);
-        (int, int) t2 = (1024, 768);
-        (int, int) t3 = (800, 600);
-        var streamToBe = new List<(int, int)> { t1, t2, t3 };
+        //var pattern = @"(?<startEle><(?<start>\w+)(.*?)?)(?<innerText>[\w \n]+)(?<end><\/\k<start>>)";
+        String input = "<div> <p>A <b>regular expression</b>, <b>regex</b> or <b>regexp</b> (sometimes called a <b>rational expression</b>) is, in <a href=\"https://en.wikipedia.org/wiki/Theoretical_computer_science\" title=\"Theoretical computer science\">theoretical computer science</a> and <a href=\"https://en.wikipedia.org/wiki/Formal_language\" title=\"Formal language\">formal language</a> theory, a sequence of <a href=\"https://en.wikipedia.org/wiki/Character_(computing)\" title=\"Character (computing)\">characters</a> that define a <i>search <a href=\"https://en.wikipedia.org/wiki/Pattern_matching\" title=\"Pattern matching\">pattern</a></i>. Usually this pattern is then used by <a href=\"https://en.wikipedia.org/wiki/String_searching_algorithm\" title=\"String searching algorithm\">string searching algorithms</a> for \"find\" or \"find and replace\" operations on <a href=\"https://en.wikipedia.org/wiki/String_(computer_science)\" title=\"String (computer science)\">strings</a>.</p> </div>";
+        var streamToBe = new List<String> { "theoretical computer science", "formal language", "characters", "pattern", "string searching algorithms", "strings" };
 
 
         // When
-        var result = RegExpr.Resolution(stream);
+        var result = RegExpr.InnerText(input, "a");
 
         // Then
         Assert.Equal(streamToBe, result);
