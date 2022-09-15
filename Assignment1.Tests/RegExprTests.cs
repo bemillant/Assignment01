48fdf583fd3e28fa29e7920db9173c9cb9c8bc54
eikbo <eikb@itu.dk>
2022-09-15 14:12:31 +0200

    Regex Done

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -57,11 +57,28 @@ public class RegExprTests
         // Given
 
         String input = @"<div> <p>The phrase <i>regular expressions</i> (and consequently, regexes) is often used to mean the specific, standard textual syntax for representing <u>patterns</u> that matching <em>text</em> need to conform to.</p> </div>";
-        var streamToBe = new List<String> { "theoretical computThe phrase regular expressions (and consequently, regexes) is often used to mean the specific, standard textual syntax for representing patterns that matching text need to conform to." };
+        var streamToBe = new List<String> { "The phrase regular expressions (and consequently, regexes) is often used to mean the specific, standard textual syntax for representing patterns that matching text need to conform to." };
 
 
         // When
         var result = RegExpr.InnerText(input, "p");
+
+        // Then
+        Assert.Equal(streamToBe, result);
+    }
+
+        [Fact]
+
+    public void InnerText_htmlInnerText_with_tag_a_should_return_stream_of_string()
+    {
+        // Given
+
+        String input = "<div> <p>A <b>regular expression</b>, <b>regex</b> or <b>regexp</b> (sometimes called a <b>rational expression</b>) is, in <a href=\"https://en.wikipedia.org/wiki/Theoretical_computer_science/\" title=\"Theoretical computer science\">theoretical computer science</a> and <a href=\"https://en.wikipedia.org/wiki/Formal_language/\" title=\"Formal language\">formal language</a> theory, a sequence of <a href=\"https://en.wikipedia.org/wiki/Character_(computing)/\" title=\"Character (computing)\">characters</a> that define a <i>search <a href=\"https://en.wikipedia.org/wiki/Pattern_matching/\" title=\"Pattern matching\">pattern</a></i>. Usually this pattern is then used by <a href=\"https://en.wikipedia.org/wiki/String_searching_algorithm/\" title=\"String searching algorithm\">string searching algorithms</a> for \"find\" or \"find and replace\" operations on <a href=\"https://en.wikipedia.org/wiki/String_(computer_science)/\" title=\"String (computer science)\">strings</a>.</p></div>\";";
+        var streamToBe = new List<String> {"theoretical computer science", "formal language", "characters", "pattern", "string searching algorithms", "strings"};
+
+
+        // When
+        var result = RegExpr.InnerText(input, "a");
 
         // Then
         Assert.Equal(streamToBe, result);
